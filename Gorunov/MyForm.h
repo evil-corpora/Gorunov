c3bf1a718aa8db55c526a7daad110c121d6dc1ce
Evil <yan.060904@mail.ru>
2023-11-28 12:02:59 +0300

    Оформил без функционала 3 лабу

--- Gorunov/MyForm.h
+++ Gorunov/MyForm.h
@@ -50,6 +50,17 @@ namespace podolsky {
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::ToolStripMenuItem^ лЅ24ToolStripMenuItem;
+
+	private: System::Windows::Forms::GroupBox^ groupBox3;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox4;
 
 	private:
 		/// <summary>
@@ -77,22 +88,34 @@ namespace podolsky {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->лЅ24ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
 			this->menuStrip1->SuspendLayout();
 			this->groupBox1->SuspendLayout();
 			this->groupBox2->SuspendLayout();
+			this->groupBox3->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// menuStrip1
 			// 
 			this->menuStrip1->GripMargin = System::Windows::Forms::Padding(2, 2, 0, 2);
 			this->menuStrip1->ImageScalingSize = System::Drawing::Size(24, 24);
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
 				this->лЅ22ToolStripMenuItem,
-					this->лЅ23ToolStripMenuItem
+					this->лЅ23ToolStripMenuItem, this->лЅ24ToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1086, 33);
+			this->menuStrip1->Size = System::Drawing::Size(1291, 33);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -207,23 +230,127 @@ namespace podolsky {
 			this->textBox2->TabIndex = 0;
 			this->textBox2->Text = L"AsEпќрќк";
 			// 
+			// лЅ24ToolStripMenuItem
+			// 
+			this->лЅ24ToolStripMenuItem->Name = L"лЅ24ToolStripMenuItem";
+			this->лЅ24ToolStripMenuItem->Size = System::Drawing::Size(79, 29);
+			this->лЅ24ToolStripMenuItem->Text = L"ЋЅ 2.4";
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Controls->Add(this->textBox5);
+			this->groupBox3->Controls->Add(this->textBox4);
+			this->groupBox3->Controls->Add(this->label4);
+			this->groupBox3->Controls->Add(this->button4);
+			this->groupBox3->Controls->Add(this->button3);
+			this->groupBox3->Controls->Add(this->label3);
+			this->groupBox3->Controls->Add(this->label2);
+			this->groupBox3->Controls->Add(this->dataGridView1);
+			this->groupBox3->Location = System::Drawing::Point(753, 37);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(378, 217);
+			this->groupBox3->TabIndex = 4;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"«адача на двумерный массивв";
+			this->groupBox3->UseWaitCursor = true;
+			this->groupBox3->Enter += gcnew System::EventHandler(this, &MyForm::groupBox3_Enter);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(6, 29);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 62;
+			this->dataGridView1->RowTemplate->Height = 28;
+			this->dataGridView1->Size = System::Drawing::Size(160, 150);
+			this->dataGridView1->TabIndex = 0;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick_1);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(172, 47);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(30, 20);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"R=";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(173, 71);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(29, 20);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"C=";
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(172, 103);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(97, 38);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"инициал";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->UseWaitCursor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Enabled = false;
+			this->button4->Location = System::Drawing::Point(172, 144);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(97, 35);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"–ешить";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(172, 194);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(57, 20);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L"ќтвет";
+			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(209, 39);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 26);
+			this->textBox4->TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(209, 71);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 26);
+			this->textBox5->TabIndex = 7;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1086, 386);
+			this->ClientSize = System::Drawing::Size(1291, 483);
+			this->Controls->Add(this->groupBox3);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
 			this->Text = L"ќќяи—ѕ Ц  урсовой проект- гр. 22¬√1 Ц ѕодольский ян ѕавлович";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
 			this->groupBox2->ResumeLayout(false);
 			this->groupBox2->PerformLayout();
+			this->groupBox3->ResumeLayout(false);
+			this->groupBox3->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -247,6 +374,18 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	String^ string = safe_cast<String^> (this->textBox2->Text);
 	std::string str2 = msclr::interop::marshal_as<std::string>(string);
 	this->textBox3->Text = Convert::ToString(kol(str2));
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+private: System::Void groupBox3_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void dataGridView1_CellContentClick_1(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 #pragma endregion
